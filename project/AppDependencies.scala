f6c6a3d7007afffefc124230d0fa9f33d528b06c
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-05-19 13:31:19 +0100

    boostrap 4 and dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,13 +6,13 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "2.24.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "4.0.0",
     "com.github.fge"          %  "json-schema-validator"    % "2.2.6"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "2.24.0" % Test,
-    "org.scalatest"           %% "scalatest"                % "3.1.2"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "4.0.0" % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.8"                 % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it"
