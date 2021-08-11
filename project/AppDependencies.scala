4128968b2d2e7b8915297069fee46c21da98af5d
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-08-11 15:04:15 +0100

    upgrade to 2.8

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,15 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "5.8.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.10.0",
     "com.github.fge"          %  "json-schema-validator"    % "2.2.6"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "5.3.0" % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.10.0" % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.8"                 % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"                 % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test, it"
   )
 }
