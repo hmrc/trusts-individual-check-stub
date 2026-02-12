c3c24205f1d5a680b69dc0d5c33b2e2758b86a3b
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-02-12 15:08:06 +0000

    DDCE-8094: Updating JSON Schema Validation Library

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ object AppDependencies {
   val bootstrapVersion = "10.3.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "com.github.fge" % "json-schema-validator"     % "2.2.14"
+    "uk.gov.hmrc"                %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "com.github.java-json-tools" % "json-schema-validator"     % "2.2.14"
   )
 
   val test: Seq[ModuleID] = Seq(
